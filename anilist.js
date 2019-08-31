d98155aa1f03fa2c15eb57414d5bdedd4919c488
Isaiah Fuller <fuller.spectrum@gmail.com>
2019-08-31 15:36:30 -0400

    Moved anime functions, added pagination to search

--- anilist.js
+++ anilist.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
-
+const Discord = require("discord.js");
+var msg = '';
 var standalone = false;
 
 function handleResponse(response) {
@@ -21,23 +22,24 @@ function handleError(error) {
 const params = process.argv;
 
 /* The anime section. Don't want to mix anime and manga. */
-function searchTitle(title, page){
+function searchTitle(title, page, m){
+    msg = m;
     var searchQuery = `
     query ($page: Int, $perPage: Int, $search: String) {
     Page (page: $page, perPage: $perPage) {
         pageInfo {
-        total
-        currentPage
-        lastPage
-        hasNextPage
-        perPage
+            total
+            currentPage
+            lastPage
+            hasNextPage
+            perPage
         }
         media (search: $search, type: ANIME) {
-        id
-        title {
-            romaji
-        }
-        format
+            id
+            title {
+                romaji
+            }
+            format
         }
     }
     }
@@ -128,7 +130,119 @@ else{
         searchId(params[3])
 }
 
+
+function aniEmbed(res){
+    var description = "No description given.";
+    var episodes = "Unknown";
+    var source = "Unknown";
+    var season = "Unknown";
+    var studio = "Unknown";
+    var format = "Unknown";
+    if(res.description)
+      description = textFilter(res.description);
+    if(res.episodes) episodes = res.episodes + " Episode";
+    if(res.episodes > 1) episodes += "s";
+    episodes += " | ";
+    if(description.length > 175) description = description.substring(0,175) + "...";
+    if(res.source)
+      var source = res.source.substring(0,1) + res.source.substring(1).toLowerCase() + " ";
+    if(res.format){
+          if(res.format == "MOVIE"){
+            format = res.format.substring(0,1) + res.format.substring(1).toLowerCase();
+            episodes = "";
+            season = "";
+        } else
+            format = res.format;
+    }
+    if(res.studios.nodes.length > 0)
+        studio = res.studios.nodes[0].name
+    if(res.season)
+        season = res.season.substring(0,1) + res.season.substring(1).toLowerCase() + " ";
+    var embed = new Discord.RichEmbed()
+    .setTitle(res.title.romaji)
+    .setDescription(episodes + season + res.startDate.year + " | " + format)
+    .addField("Description",description)
+    .addField("Score",res.averageScore+"/100",true)
+    .addField("Source",source,true)
+    .addField("Studio",studio,true)
+    .addField("Duration",res.duration+" minutes",true)
+    .setThumbnail(res.coverImage.medium)
+    .setURL("https://anilist.co/anime/"+res.id)
+    .setFooter("AniList.co Search")
+    .setTimestamp();
+    return embed;
+  }
+
+function search(res){
+  x = "";
+  console.log(res.data)
+  res.data.Page.media.forEach(i => {
+    x += i.title.romaji + " \nFormat: " + i.format + " | " + "https://anilist.co/anime/" + i.id + " \n\n\u200B";
+  });
+  var result;
+  msg.channel.send(x)
+  .then(async function(rep) {
+    result = rep;
+    var fil1 = (rc, u) => rc.emoji.name === '1⃣' && u.id === msg.author.id;
+    var fil2 = (rc, u) => rc.emoji.name === '2⃣' && u.id === msg.author.id;
+    var fil3 = (rc, u) => rc.emoji.name === '3⃣' && u.id === msg.author.id;
+    var fil4 = (rc, u) => rc.emoji.name === '⬅' && u.id === msg.author.id;
+    var fil5 = (rc, u) => rc.emoji.name === '➡' && u.id === msg.author.id;
+    result.awaitReactions(fil1, {max: 1}).then(function(){
+        searchId(res.data.Page.media[0].id).then(function(r){
+            console.log(r)
+            msg.channel.send(aniEmbed(r.data.Media));
+            result.delete();
+        })
+    })
+    result.awaitReactions(fil2, {max: 1}).then(function(){
+        searchId(res.data.Page.media[1].id).then(function(r){
+            console.log(r)
+            msg.channel.send(aniEmbed(r.data.Media));
+            result.delete();
+        })
+    })
+    result.awaitReactions(fil3, {max: 1}).then(function(){
+        searchId(res.data.Page.media[2].id).then(function(r){
+            console.log(r)
+            msg.channel.send(aniEmbed(r.data.Media));
+            result.delete();
+        })
+      })
+    result.awaitReactions(fil4, {max: 1}).then(function(){
+        result.delete();
+        searchTitle(msg.content.substring(14), res.data.Page.pageInfo.currentPage - 1, msg).then(function(res2){
+            search(res2);
+        });
+    })
+    result.awaitReactions(fil5, {max: 1}).then(function(){
+        result.delete();
+        searchTitle(msg.content.substring(14), res.data.Page.pageInfo.currentPage + 1, msg).then(function(res2){
+            search(res2);
+        });
+    })
+    if(res.data.Page.media.length > 0)
+        await result.react('1⃣');
+    if(res.data.Page.media.length > 1)
+        await result.react('2⃣');
+    if(res.data.Page.media.length > 2)
+        await result.react('3⃣');
+    if(res.data.Page.pageInfo.currentPage > 1)
+        await result.react('⬅');
+    if(res.data.Page.pageInfo.hasNextPage)
+        await result.react('➡');
+  }); 
+}
+
+// I'll probably have to add more to this
+function textFilter(text){
+    text = text.replace(/<br>/g,"\n");
+    return text;
+}
+
 module.exports = {
     searchTitle,
-    searchId
+    searchId,
+    aniEmbed,
+    search
   }
--- index.js
+++ index.js
@@ -12,61 +12,27 @@ client.on('message', msg => {
   if(msg.content.startsWith("!music")){
     console.log("PLAY");
     var com = msg.content.substring(7);
-    if(com.startsWith("play") || msg.content.startsWith("play"))
-      if(com.includes("youtube.com/watch?v=") || com.includes("youtu.be"))
+    if(com.startsWith("play"))
+      if(com.includes("youtube.com/watch?v=") || msg.content.includes("youtu.be"))
         Music.main(msg, com.substring(5));
       else
         Music.resume();
-    if(com.startsWith("skip") || msg.content.startsWith("stop"))
+    if(com.startsWith("skip"))
       Music.end();
-    if(com.startsWith("pause") || msg.content.startsWith("pause"))
+    if(com.startsWith("pause"))
       Music.pause();
-    if(com.startsWith("stop") || msg.content.startsWith("stop"))
+    if(com.startsWith("stop"))
       Music.stop();
+    if(com.startsWith("des"))
+      msg.channel.send(Music.isDestroyed());
   }
   if(msg.content.startsWith("!anime")){
     console.log("ANIME");
     if(msg.content.substring(7).startsWith("title")){
       console.log("TITLE");
       var com = msg.content.substring(14);
-      var dataProm = anilist.searchTitle(com, 1);
-// Well this went downhill fast... Learned how to make promises though lol
-      dataProm.then(function(res) {
-        x = "";
-        res.data.Page.media.forEach(i => {
-          x += i.title.romaji + " \nFormat: " + i.format + " | " + "https://anilist.co/anime/" + i.id + " \n\n\u200B";
-        });
-        var result;
-        msg.channel.send(x)
-        .then(async function(r) {
-          result = r;
-          await result.react('1⃣');
-          await result.react('2⃣');
-          await result.react('3⃣');
-          //await res.react('⬅');
-          await result.react('➡');
-          var fil1 = (rc, u) => rc.emoji.name === '1⃣' && u.id === msg.author.id;
-          var fil2 = (rc, u) => rc.emoji.name === '2⃣' && u.id === msg.author.id;
-          var fil3 = (rc, u) => rc.emoji.name === '3⃣' && u.id === msg.author.id;
-          result.awaitReactions(fil1, {max: 1}).then(function(){
-            anilist.searchId(res.data.Page.media[0].id).then(function(r){
-              msg.channel.send(aniEmbed(r.data.Media));
-              result.delete();
-            })
-          })
-          result.awaitReactions(fil2, {max: 1}).then(function(){
-            anilist.searchId(res.data.Page.media[1].id).then(function(r){
-              msg.channel.send(aniEmbed(r.data.Media));
-              result.delete();
-            })
-          })
-          result.awaitReactions(fil3, {max: 1}).then(function(){
-            anilist.searchId(res.data.Page.media[2].id).then(function(r){
-              msg.channel.send(aniEmbed(r.data.Media));
-              result.delete();
-            })
-          })
-        });
+      anilist.searchTitle(com, 1, msg).then(function(res){
+        anilist.search(res);
       })
     }
     if(msg.content.substring(7).startsWith("id")){
@@ -75,71 +41,10 @@ client.on('message', msg => {
       var dataProm = anilist.searchId(Number(com));
       dataProm.then(function(res) {
         console.log(res)
-        msg.channel.send(aniEmbed(res.data.Media));
+        msg.channel.send(anilist.aniEmbed(res.data.Media));
       })
     }
   }
-  if(msg.content.startsWith("!play"))
-    if(msg.content.includes("youtube.com/watch?v=") || msg.content.includes("youtu.be"))
-      Music.main(msg, msg.content.substring(5));
-    else
-      Music.resume();
-  if(msg.content.startsWith("!skip"))
-    Music.end();
-  if(msg.content.startsWith("!stop"))
-    Music.stop();
-  if(msg.content.startsWith("!pause"))
-    Music.pause();
-  if(msg.content.startsWith("!des"))
-    msg.channel.send(Music.isDestroyed());
 });
-
-// I will have to clean this up, but I need it working first
-function aniEmbed(res){
-  if(res.description)
-    var description = textFilter(res.description);
-  else
-    var description = "No description given."
-  if(res.episodes)
-    var episodes = res.episodes + " Episode";
-  else
-    var episodes = "N/A Episodes"
-  if(res.episodes > 1) episodes += "s";
-  episodes += " | ";
-  if(description.length > 175){
-    description = description.substring(0,175) + "...";
-  }
-  var season = res.season.substring(0,1) + res.season.substring(1).toLowerCase() + " ";
-  if(res.source)
-    var source = res.source.substring(0,1) + res.source.substring(1).toLowerCase() + " ";
-  else
-    var source = "N/A"
-  if(res.format == "MOVIE"){
-    var format = res.format.substring(0,1) + res.format.substring(1).toLowerCase();
-    episodes = "";
-    season = "";
-  }
-  else
-    var format = res.format;
-  var embed = new Discord.RichEmbed()
-  .setTitle(res.title.romaji)
-  .setDescription(episodes + season + res.startDate.year + " | " + format)
-  .addField("Description",description)
-  .addField("Score",res.averageScore+"/100",true)
-  .addField("Source",source,true)
-  .addField("Studio",res.studios.nodes[0].name,true)
-  .addField("Duration",res.duration+" minutes",true)
-  .setThumbnail(res.coverImage.medium)
-  .setURL("https://anilist.co/anime/"+res.id)
-  .setFooter("AniList.co Search")
-  .setTimestamp();
-  return embed;
-}
-
-// I'll probably have to add more to this
-function textFilter(text){
-  text = text.replace(/<br>/g,"\n");
-  return text;
-}
 
 client.login(process.env.DISCORD_TOKEN);
